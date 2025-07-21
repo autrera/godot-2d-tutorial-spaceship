788a64cc5a0f9749f4f5cb68f031c39676467d89
Aldo Utrera <accounts@aldoutrera.com>
2025-07-21 16:19:49 -0500

    Make the ship steer smoothly

--- lessons/ship.gd
+++ lessons/ship.gd
@@ -1,9 +1,11 @@
 extends Sprite2D
 
-var velocity := Vector2(0, 0)
-var normal_speed := 600.0
 var boost_speed := 1500.0
+var normal_speed := 600.0
+
 var max_speed := normal_speed
+var velocity := Vector2(0, 0)
+var steering_factor := 10.0
 
 
 func _process(delta: float) -> void:
@@ -15,10 +17,12 @@ func _process(delta: float) -> void:
 		direction = direction.normalized()
 
 	if Input.is_action_just_pressed("boost"):
-		%Timer.start()
 		max_speed = boost_speed
+		get_node("Timer").start()
 
-	velocity = direction * max_speed
+	var desired_velocity := max_speed * direction
+	var steering_vector := desired_velocity - velocity
+	velocity += steering_vector * steering_factor * delta
 	position += velocity * delta
 
 	if direction.length() > 0.0:
